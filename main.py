15d295780e5e56b04da7d63ecc4f785a413f5928
gaurav parajuli <gparajuli.1997@gmail.com>
2023-05-21 15:06:26 +0545

    completely implemented top50 books view

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+from email.mime import image
 from flask import Flask, render_template
+
+import pickle
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    top50 = pickle.load(open('model/top50books.pkl', 'rb'))
+    return render_template('index.html', title=top50['Book-Title'].to_list(), author=top50['Book-Author'].to_list(), image=top50['Image-URL-M'].to_list(), vote=top50['num_ratings'].to_list(), rating=[round(x, 1) for x in top50['avg_ratings'].to_list()])
+
 
 @app.route('/recommend')
 def recommend():
     return render_template('recommendations.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
